3694b2ff5d9e108bf56b5ff29dce865da3492701
张琼娜 <gogoshine168@gmail.com>
2025-09-04 10:15:00 +0800

    基础搭建

---
+++ my-docs-site/docs/.vitepress/config.ts
@@ -0,0 +1,23 @@
+
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    title: '简创AIGC官方文档', // 网站标题
+    themeConfig: {
+        sidebar: [ // 侧边栏配置
+            {
+                text: '指南', // 导航菜单标题
+                items: [
+                    { text: '介绍', link: '/guide' }, // 对应 docs/guide.md
+                    // {
+                    //     text: '二级操作', items: [
+                    //         { text: '文档1', link: '/first/guide' }, // 对应 docs/guide.md
+                    //         { text: '文档2', link: '/first/api' }     // 对应 docs/api.md
+                    //     ]
+                    // }
+                ]
+            }
+        ]
+    }
+})
