dadaa005f9c89d731eb34ab297ebb3279cf7d116
张琼娜 <gogoshine168@gmail.com>
2025-09-04 11:17:16 +0800

    增加logo和语言配置

--- my-docs-site/docs/.vitepress/config.ts
+++ my-docs-site/docs/.vitepress/config.ts
@@ -3,21 +3,28 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
+    lang: 'zh-CN', // 语言
     title: '简创AIGC官方文档', // 网站标题
     themeConfig: {
+        logo: '/logo.png', // 网站图标
+        // 文档页脚（上一页/下一页）
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
         sidebar: [ // 侧边栏配置
             {
                 text: '指南', // 导航菜单标题
                 items: [
                     { text: '介绍', link: '/docs/guide' }, // 对应 docs/guide.md
-                    // {
-                    //     text: '二级操作', items: [
-                    //         { text: '文档1', link: '/docs/first/guide' }, // 对应 docs/guide.md
-                    //         { text: '文档2', link: '/docs/first/api' }     // 对应 docs/api.md
-                    //     ]
-                    // }
                 ]
             }
         ],
+        footer: {
+            // 版权前显示的信息
+            // message: 'Released under the MIT License.',
+            // 实际的版权文本
+            copyright: '版权所有 © 2025 简创AIGC'
+        }
     }
 })
---
+++ my-docs-site/docs/docs/guide.md
@@ -0,0 +1,9 @@
+# 介绍
+
+<!-- 插入网络图片 -->
+
+![Vue.js Logo](https://vuejs.org/images/logo.png)
+
+<!-- 插入本地图片 -->
+
+![图片引入示例](./demo.png)
