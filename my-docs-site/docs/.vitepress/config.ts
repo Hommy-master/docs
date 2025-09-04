8ca2f43c0d62ca213bf71946b1ccdcceaa04c79b
张琼娜 <gogoshine168@gmail.com>
2025-09-04 10:36:24 +0800

    增加插入图片功能示例

--- my-docs-site/docs/.vitepress/config.ts
+++ my-docs-site/docs/.vitepress/config.ts
@@ -9,15 +9,15 @@ export default defineConfig({
             {
                 text: '指南', // 导航菜单标题
                 items: [
-                    { text: '介绍', link: '/guide' }, // 对应 docs/guide.md
+                    { text: '介绍', link: '/docs/guide' }, // 对应 docs/guide.md
                     // {
                     //     text: '二级操作', items: [
-                    //         { text: '文档1', link: '/first/guide' }, // 对应 docs/guide.md
-                    //         { text: '文档2', link: '/first/api' }     // 对应 docs/api.md
+                    //         { text: '文档1', link: '/docs/first/guide' }, // 对应 docs/guide.md
+                    //         { text: '文档2', link: '/docs/first/api' }     // 对应 docs/api.md
                     //     ]
                     // }
                 ]
             }
-        ]
+        ],
     }
 })
