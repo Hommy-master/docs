4dfd787d55fb432393a96e274e7a2f77308c121d
张琼娜 <gogoshine168@gmail.com>
2025-09-05 11:28:59 +0800

    增加汉化

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,31 +3,37 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
-    lang: 'zh-CN', // 语言
     title: '简创AIGC官方文档', // 网站标题
+    locales: { root: { label: '简体中文', lang: 'zh-CN' } },
     themeConfig: {
         logo: '/logo.png', // 网站图标
+        // 汉化 begin
         outlineTitle: '当前页导航', // 右侧大纲标题
         // 文档页脚（上一页/下一页）
         docFooter: {
             prev: '上一页',
             next: '下一页'
         },
+        lastUpdated: { text: '最后更新于' },
+        darkModeSwitchLabel: '深色模式',
+        sidebarMenuLabel: '菜单',
+        returnToTopLabel: '返回顶部',
+        // 汉化 end
         sidebar: [ // 侧边栏配置
             {
                 text: '指南', // 导航菜单标题
                 collapsed: false,
                 items: [
                     { text: '介绍', link: '/docs/guide' }, // 对应 docs/guide.md
-                    { text: '快速开始', link: '/docs/guide1' }, // 对应 docs/guide/getting-started.md
+                    { text: '快速开始', link: '/docs/guide1' },
                 ]
             },
             {
                 text: '指南2', // 导航菜单标题
                 collapsed: false,
                 items: [
-                    { text: '快速开始2', link: '/docs/guide2' }, // 对应 docs/guide/getting-started.md
-                    { text: '快速开始3', link: '/docs/guide3' }, // 对应 docs/guide/getting-started.md
+                    { text: '快速开始2', link: '/docs/guide2' },
+                    { text: '快速开始3', link: '/docs/guide3' },
                 ]
             }
         ],
